b7e82795f4a90ec947a9ca566554fd4f7d21ce23
angelmae emeterio <emeterioangelmae@gmail.com>
2025-04-07 15:23:46 +0800

    fix

---
+++ src/views/auth/RegisterView.vue
@@ -0,0 +1,43 @@
+<script setup>
+import AppLayout from '@/components/layout/AppLayout.vue';
+import RegisterForm from '@/components/auth/RegisterForm.vue';
+import { useDisplay } from 'vuetify'
+
+const { mobile } = useDisplay()
+</script>
+
+<template>
+<AppLayout>
+  <template #content>
+    <v-container fluid>
+
+         <v-row>
+          <v-col cols="12" md="8" class="bg-surface-light h-screen" v-if="!mobile"> </v-col>
+          <v-col cols="12" md="4" :class="mobile ? '' : 'pt-16' ">
+            <v-card class="mx-auto" elevation="0" max-width="600">
+                <v-card-title class="text-center">
+                  <v-img class="mx-auto" src="/images/logo-fabrique.png"
+                  :width="mobile ? '75%' : '25%'"
+                  ></v-img>
+                  <h3 class="font-weight-black ">Fabrique</h3>
+                  <p class="font-weight-bold">Registration Form</p>
+
+                </v-card-title>
+
+                <v-card-text class="pt-4">
+                  <v-divider class="my-5"></v-divider>
+
+                  <RegisterForm> </RegisterForm>
+                  <v-divider class="my-5"></v-divider>
+
+                  <h5 class="text-center">Already have account? <RouterLink class="text-pink-lighten-2 font-weight-black" to="/">Click here to Login</RouterLink></h5>
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+  </template>
+
+</AppLayout>
+
+</template>
