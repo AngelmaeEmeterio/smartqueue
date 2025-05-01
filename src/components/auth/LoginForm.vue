8d622b53773c6d68120b8495ee92f3be5871dab0
angelmae emeterio <emeterioangelmae@gmail.com>
2025-05-01 10:56:15 +0800

    feat: added src folder

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -13,7 +13,7 @@ const isPasswordVisible = ref(false)
                       prepend-inner-icon="mdi-lock"
                       label="Password"
                       :type="isPasswordVisible ? 'text' : 'password'"
-                      :append-inner-icon="isPasswordVisible ? 'mdi-eye-off' : 'mdi-eye' "
+                      :append-inner-icon="isPasswordVisible ? 'mdi-eye-off' : 'mdi-eye'"
                       @click:append-inner="isPasswordVisible = !isPasswordVisible"
                     ></v-text-field>
 
